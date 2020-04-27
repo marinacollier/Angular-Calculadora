7c960a2fb262b978988dc7d8d58d318fe64bf62d
Marina Collier <marinacollieramelo@gmail.com>
2020-04-27 12:39:57 -0300

    first commit

---
+++ calculadora/src/app/app.component.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
+
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CalculadoraModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
---
+++ calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { CalculadoraService } from './calculadora.service';
+
+describe('CalculadoraService', () => {
+  let service: CalculadoraService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalculadoraService);
+  });
+
+});
